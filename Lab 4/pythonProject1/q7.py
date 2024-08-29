675af8621a1297a87c76a52a082ff5e282d06728
Shuvam Chakraborty <166750192+ShuvamChakraborty-B@users.noreply.github.com>
2024-08-29 23:33:52 +0530

    Delete Lab 4/pythonProject1 directory

--- Lab 4/pythonProject1/q7.py
+++
@@ -1,9 +0,0 @@
-def digit_count():
-    inp=input("Enter a string: ")
-    j=inp.split()
-    count=[]
-    for words in j:
-        if words.isdigit():
-            count.append(words)
-    print(count)
-digit_count()
